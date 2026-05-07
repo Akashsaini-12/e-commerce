0529b7d3daa6dd3eaadde01e68818641eefe7bd1
Akashsaini-12 <akashsaini5377@gmail.com>
2026-05-08 01:07:32 +0530

    setup deployment pipeline

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,11 +1,11 @@
 // const API_BASE = process.env.REACT_APP_API_BASE_URL || "https://website-backend-bot8.vercel.app";
   //  const API_BASE = "https://website-backend-bot8.vercel.app";
   // const API_BASE = "http://35.244.32.175:4000";
-  // const API_BASE = "https://api.smalcouture.com";
+  const API_BASE = "https://api.smalcouture.com";
 // ss
-const API_BASE =
-   process.env.REACT_APP_API_BASE_URL ||
-   `http://${window.location.hostname}:4000`;
+// const API_BASE =
+//    process.env.REACT_APP_API_BASE_URL ||
+//    `http://${window.location.hostname}:4000`;
 async function fetchJson(url, options = {}, timeoutMs = 30000) {
   const controller = new AbortController();
   const t = setTimeout(() => controller.abort(), timeoutMs);
